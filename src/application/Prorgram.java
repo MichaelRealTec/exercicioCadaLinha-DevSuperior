b259a40a5ec2e9924bb1a849b0faeef3e1b7eca5
Michael Rafael de Andrade <realteceinformatica@gmail.com>
2024-07-22 19:08:00 -0300

    exercicio resolvido - DevSuperior

--- src/application/Prorgram.java
+++ src/application/Prorgram.java
@@ -1,10 +1,43 @@
 package application;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Prorgram {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// exercicioCadaLinha-DevSuperior
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
 
+		int n;
+		
+
+		
+		System.out.print("Qual a ordem da matriz? ");
+		n = sc.nextInt();
+		
+		int[] maiorElementoLinha = new int[n];
+		
+		int[][] matriz = new int[n][n];
+		for(int i = 0; i < matriz.length; i++) {
+			for(int j = 0; j < matriz[i].length; j++) {
+				System.out.printf("Elemento [%d,%d]: ",i,j);
+				matriz[i][j] = sc.nextInt();
+				if(maiorElementoLinha[i] < matriz[i][j]) {
+					maiorElementoLinha[i] = matriz[i][j];
+				}
+			}
+		}
+		
+		System.out.println("MAIOR ELEMENTO DE CADA LINHA: ");
+		for(int i = 0; i < matriz.length; i++) {
+			System.out.println(maiorElementoLinha[i]);
+			
+		}
+		
+		sc.close();
 	}
 
 }
